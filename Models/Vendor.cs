248220e02fa2aca7ea490f7806324a726ab287cf
takenowaki <takenowaki.t@gmail.com>
2024-05-01 18:18:59 +0900

    DBの追加

---
+++ Models/Vendor.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+[Table("m_vender")]
+public class Vender
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("vender_index")]
+    public int VenderIndex { get; set; }
+    [Required]
+    [Column("vender_name")]
+    [MaxLength(100)]
+    public string VenderName { get; set; }
+    [Column("create_date")]
+    public DateTime? CreateDate { get; set; }
+    [Column("create_user")]
+    public int? CreateUser { get; set; }
+    [Column("update_date")]
+    public DateTime? UpdateDate { get; set; }
+    [Column("update_user")]
+    public int? UpdateUser { get; set; }
+    [Required]
+    [Column("delete_flag")]
+    public int DeleteFlag { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using YasiroRegrave.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+       options.UseSqlServer(
+           builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,11 +20,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
