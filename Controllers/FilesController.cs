8b3e10135767f852f3c5fd90c826fd09f5fc18cb
koyama <koyama@technosphere.co.jp>
2024-06-05 18:17:39 +0900

    file controller暫定対応

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -1,7 +1,7 @@
-using CrayonBookSystem.Data;
-using CrayonBookSystem.Pages.Common;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
+using YasiroRegrave.Data;
+using YasiroRegrave.Pages.common;
 
 namespace CrayonBookSystem
 {
@@ -15,7 +15,7 @@ namespace CrayonBookSystem
     [ApiController]
     public class FilesController : ControllerBase
     {
-        protected readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
         private string caseStudyFilePath = "";    //実践事例のファイルパス
         private string trainingFilePath = "";    //研修資料のファイルパス
 
@@ -53,26 +53,6 @@ namespace CrayonBookSystem
 
             switch (select)
             {
-                case SelectFile.ファイル:
-                    //拡張子取得
-                    var CaseStudies = _context.CaseStudies
-                            .Where(cs => cs.CaseStudyId == id && cs.ReleaseStatus != 0)
-                            .Select(cs => new { cs.ReleaseStatus, cs.FileName })
-                            .FirstOrDefault();
-                    if (CaseStudies == null)
-                    {
-                        return Results.Text("ファイルが存在しません。", "text/plain", System.Text.Encoding.UTF8);
-                    }
-                    if (CaseStudies.ReleaseStatus == 1 && Utils.IsValidUser(HttpContext, _context).Item1 == false)
-                    {
-                        return Results.Text("ファイルへのアクセス権限がありません。", "text/plain", System.Text.Encoding.UTF8);
-                    }
-
-                    string fileEx = Path.GetExtension(CaseStudies.FileName);
-                    string filePath = string.Format(caseStudyFilePath, id, fileEx);
-
-                    return ResultDocument(filePath, fileEx, CaseStudies.FileName); //ダウンロードファイル名はタイトルの方が良いかも
-
                 case SelectFile.画像:
                     string imgEx = "";
                     string imgPath = "";
@@ -93,43 +73,6 @@ namespace CrayonBookSystem
                     return Results.Text("不正なアクセスです。", "text/plain", System.Text.Encoding.UTF8);
 
             }
-        }
-
-        [HttpGet]
-        public IResult? Traning()
-        {
-
-            int tid = -1;
-            int tcid = -1;
-
-            if (!(Request.Query["tid"].ToString() != "" && int.TryParse(Request.Query["tid"].ToString(), out tid))
-                || !(Request.Query["tcid"].ToString() != "" && int.TryParse(Request.Query["tcid"].ToString(), out tcid)))
-            {
-                return Results.Text("不正なアクセスです。", "text/plain", System.Text.Encoding.UTF8);
-            }
-
-            //拡張子取得
-            var Trainings = _context.Trainings
-                    .GroupJoin(_context.TrainingContents, t => t.TrainingId, tc => tc.TrainingId, (t, tc) => new { t, tc })
-                    .SelectMany(
-                        sm => sm.tc.DefaultIfEmpty(),
-                        (sm, tc) => new { t = sm.t, tc = tc })
-                    .Where(w => w.t.TrainingId == tid && w.t.ReleaseStatus != 0 && w.tc.TrainingContentsId == tcid && w.tc.ContentType == 1)
-                    .Select(s => new { s.t.ReleaseStatus, s.tc.SupplementFileName })
-                    .FirstOrDefault();
-            if (Trainings == null || Trainings.SupplementFileName == null)
-            {
-                return Results.Text("ファイルが存在しません。", "text/plain", System.Text.Encoding.UTF8);
-            }
-            if (Trainings.ReleaseStatus == 1 && Utils.IsValidUser(HttpContext, _context).Item1 == false)
-            {
-                return Results.Text("ファイルへのアクセス権限がありません。", "text/plain", System.Text.Encoding.UTF8);
-            }
-
-            string fileEx = Path.GetExtension(Trainings.SupplementFileName);
-            string filePath = string.Format(trainingFilePath, tid, tcid, fileEx);
-
-            return ResultDocument(filePath, fileEx, Trainings.SupplementFileName); //ダウンロードファイル名はタイトルの方が良いかも
         }
 
         private IResult ResultImage(string imgPath, string imgEx)
