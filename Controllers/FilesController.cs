6d46893097a1bffda400e9395f357f1b332ed052
koyama <koyama@technosphere.co.jp>
2024-06-06 18:39:35 +0900

    墓所画像アップロードと表示対応

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -5,76 +5,53 @@ using YasiroRegrave.Pages.common;
 
 namespace CrayonBookSystem
 {
-    public enum SelectFile
-    {
-        ファイル,
-        画像
-    }
-
     [Route("api/[controller]/[action]/")]
     [ApiController]
     public class FilesController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
-        private string caseStudyFilePath = "";    //実践事例のファイルパス
-        private string trainingFilePath = "";    //研修資料のファイルパス
+        private string regraveFilePath = "";    //墓所画像のファイルパス
 
 
         public FilesController(ApplicationDbContext context)
         {
             _context = context;
-            caseStudyFilePath = Path.Combine(Config.DataFilesCaseStudyPath, "csid-{0}{1}"); //ファイル名はIDで格納
-            trainingFilePath = Path.Combine(Config.DataFilesTraningPath, "tid-{0}", "tcid-{1}{2}"); //ファイル名はIDで格納
+            regraveFilePath = Path.Combine(Config.DataFilesRegravePath, ""); //ファイル名はIDで格納
         }
 
         [HttpGet]
-        public IResult? CaseStudy()
+        public IResult? GraveImg()
         {
-            SelectFile? select = null;
-            int id = -1;
+            int reien = -1;
+            int area = -1;
+            int sel = -1;
+            string? kukaku = string.Empty;
 
-            if (Request.Query["sel"].ToString() != "")
-            {
-                switch (Request.Query["sel"].ToString())
-                {
-                    case "0":
-                        select = SelectFile.ファイル;
-                        break;
-                    case "1":
-                        select = SelectFile.画像;
-                        break;
-                }
-            }
-
-            if (select == null || !(Request.Query["csid"].ToString() != "" && int.TryParse(Request.Query["csid"].ToString(), out id)))
+            if (!(Request.Query["r"].ToString() != "" && int.TryParse(Request.Query["r"].ToString(), out reien)) ||
+                !(Request.Query["a"].ToString() != "" && int.TryParse(Request.Query["a"].ToString(), out area)) ||
+                !(Request.Query["sel"].ToString() != "" && int.TryParse(Request.Query["sel"].ToString(), out sel)) ||
+                !Request.Query.ContainsKey("k")
+                )
             {
                 return Results.Text("不正なアクセスです。", "text/plain", System.Text.Encoding.UTF8);
             }
 
-            switch (select)
+            kukaku = Request.Query["k"];
+            string imgEx = "";
+            string imgPath = "";
+            // 拡張子選定（負荷軽減のためDBアクセスしない）
+            foreach (string ext in Config.MIME_IMAGE.Keys)
             {
-                case SelectFile.画像:
-                    string imgEx = "";
-                    string imgPath = "";
-                    // 拡張子選定（負荷軽減のためDBアクセスしない）
-                    foreach (string ext in Config.MIME_IMAGE.Keys)
-                    {
-                        imgEx = ext;
-                        imgPath = string.Format(caseStudyFilePath, id, imgEx);
-                        if (System.IO.File.Exists(imgPath))
-                        {
-                            //ファイル確定
-                            break;
-                        }
-                    }
-                    return ResultImage(imgPath, imgEx);
-
-                default:
-                    return Results.Text("不正なアクセスです。", "text/plain", System.Text.Encoding.UTF8);
-
+                imgEx = ext;
+                imgPath = $"{regraveFilePath}\\{reien}\\{area}\\{kukaku}-{sel}{imgEx}";
+                if (System.IO.File.Exists(imgPath))
+                {
+                    //ファイル確定
+                    break;
+                }
             }
+            return ResultImage(imgPath, imgEx);
         }
-
         private IResult ResultImage(string imgPath, string imgEx)
         {
             if (System.IO.File.Exists(imgPath))
@@ -89,23 +66,6 @@ namespace CrayonBookSystem
             else
             {
                 return Results.File(@".\img\noimage.png", Config.MIME_IMAGE[".png"]);
-            }
-
-        }
-        private IResult ResultDocument(string filePath, string fileEx, string downFileName)
-        {
-            if (System.IO.File.Exists(filePath))
-            {
-                MemoryStream memoryStream = new MemoryStream();
-                using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-                {
-                    fileStream.CopyTo(memoryStream);
-                }
-                return Results.File(memoryStream.ToArray(), Config.MIME_DOCUMENT[fileEx]); //, downFileName);
-            }
-            else
-            {
-                return Results.Text("ファイルが存在しません。", "text/plain", System.Text.Encoding.UTF8);
             }
 
         }
--- Pages/CemeteryInfoEdit.cshtml.cs
+++ Pages/CemeteryInfoEdit.cshtml.cs
@@ -6,6 +6,7 @@ using System.Diagnostics.SymbolStore;
 using YasiroRegrave.Data;
 using YasiroRegrave.Model;
 using YasiroRegrave.Pages.common;
+using static YasiroRegrave.Pages.ReienListModel;
 
 namespace YasiroRegrave.Pages
 {
@@ -27,8 +28,22 @@ namespace YasiroRegrave.Pages
         [BindProperty]
         public string Image2Fname { get; set; }
 
-        //[BindProperty]
+        [BindProperty]
+        public string Image1FnameURL { get; set; }
+        [BindProperty]
+        public string Image2FnameURL { get; set; }
+        [BindProperty]
+        public IFormFile Image1 { get; set; }
+        [BindProperty]
+        public IFormFile Image2 { get; set; }
+
+        [BindProperty]
         public int? CemeteryInfoIndex { get; set; }
+
+        private string? ReienCode { get; set; }
+        private string? AreaCode { get; set; }
+        private string? SectionCode { get; set; }
+        private string? CemeteryCode { get; set; }
 
         private readonly ApplicationDbContext _context;
         public CemeteryInfoEditModel(ApplicationDbContext context)
@@ -41,37 +56,37 @@ namespace YasiroRegrave.Pages
             CemeteryInfoIndex = index;
             if (index.HasValue)
             {
-                // データベースから墓所情報を取得
-                var cemeteryinfo = _context.CemeteryInfos
-                    .Where(ci => ci.DeleteFlag == 0 && ci.CemeteryInfoIndex == index.Value)
-                    .Select(ci => new PageCemeteryInfo
-                    {
-                        CemeteryInfoIndex = ci.CemeteryInfoIndex,
-                        ReienName = ci.Cemetery.Section.Area.Reien.ReienName,
-                        AreaName = ci.Cemetery.Section.Area.AreaName,
-                        SectionName = ci.Cemetery.Section.SectionName,
-                        CemeteryName = ci.Cemetery.CemeteryName,
-                        Image1Fname = ci.Image1Fname,
-                        Image2Fname = ci.Image2Fname
-                    })
-                    .FirstOrDefault();
-
-                // 墓所情報を各プロパティに設定
-                if (cemeteryinfo != null)
-                {
-                    ReienName = cemeteryinfo.ReienName;
-                    AreaName = cemeteryinfo.AreaName;
-                    SectionName = cemeteryinfo.SectionName;
-                    CemeteryName = cemeteryinfo.CemeteryName;
-                    Image1Fname = cemeteryinfo.Image1Fname;
-                    Image2Fname = cemeteryinfo.Image2Fname;
-                }
+                GetPage(index);
             }
         }
         public IActionResult OnPost(int? index)
         {
+            GetPage(index);
             try
             {
+                if (Image1 != null)
+                {
+                    var filePath = Path.Combine(Config.DataFilesRegravePath, "");
+                    var fileExtension1 = Path.GetExtension(Image1.FileName);
+                    var imgPath = $"{filePath}\\{ReienCode}\\{AreaCode}\\{SectionCode}-{CemeteryCode}-1{fileExtension1}";
+                    using (var stream = System.IO.File.Create(imgPath))
+                    {
+                        Image1.CopyTo(stream);
+                    }
+                    Image1Fname = Image1.FileName;
+                }
+
+                if (Image2 != null)
+                {
+                    var filePath = Path.Combine(Config.DataFilesRegravePath, "");
+                    var fileExtension1 = Path.GetExtension(Image2.FileName);
+                    var imgPath = $"{filePath}\\{ReienCode}\\{AreaCode}\\{SectionCode}-{CemeteryCode}-2{fileExtension1}";
+                    using (var stream = System.IO.File.Create(imgPath))
+                    {
+                        Image2.CopyTo(stream);
+                    }
+                    Image2Fname = Image2.FileName;
+                }
                 if (index == null)
                 {
                     var newCemeteryInfo = new CemeteryInfo
@@ -105,6 +120,51 @@ namespace YasiroRegrave.Pages
             }
             return RedirectToPage("/CemeteryInfoList");
         }
+        private void GetPage(int? index)
+        {
+            // データベースから墓所情報を取得
+            var cemeteryinfo = _context.CemeteryInfos
+                .Where(ci => ci.DeleteFlag == 0 && ci.CemeteryInfoIndex == index)
+                .Select(ci => new PageCemeteryInfo
+                {
+                    CemeteryInfoIndex = ci.CemeteryInfoIndex,
+                    ReienName = ci.Cemetery.Section.Area.Reien.ReienName,
+                    AreaName = ci.Cemetery.Section.Area.AreaName,
+                    SectionName = ci.Cemetery.Section.SectionName,
+                    CemeteryName = ci.Cemetery.CemeteryName,
+                    Image1Fname = ci.Image1Fname,
+                    Image2Fname = ci.Image2Fname,
+                    ReienCode = ci.Cemetery.Section.Area.Reien.ReienCode,
+                    AreaCode = ci.Cemetery.Section.Area.AreaCode,
+                    SectionCode = ci.Cemetery.Section.SectionCode,
+                    CemeteryCode = ci.Cemetery.CemeteryCode
+                })
+                .FirstOrDefault();
+
+            // 墓所情報を各プロパティに設定
+            if (cemeteryinfo != null)
+            {
+                ReienName = cemeteryinfo.ReienName;
+                AreaName = cemeteryinfo.AreaName;
+                SectionName = cemeteryinfo.SectionName;
+                CemeteryName = cemeteryinfo.CemeteryName;
+                Image1Fname = cemeteryinfo.Image1Fname;
+                Image2Fname = cemeteryinfo.Image2Fname;
+                ReienCode = cemeteryinfo.ReienCode;
+                AreaCode = cemeteryinfo.AreaCode;
+                SectionCode = cemeteryinfo.SectionCode;
+                CemeteryCode = cemeteryinfo.CemeteryCode;
+                if (Image1Fname != "")
+                {
+                    Image1FnameURL = $"https://localhost:7147/api/Files/GraveImg?r={ReienCode}&a={AreaCode}&k={SectionCode}-{CemeteryCode}&sel=1";
+                }
+                if (Image2Fname != "")
+                {
+                    Image2FnameURL = $"https://localhost:7147/api/Files/GraveImg?r={ReienCode}&a={AreaCode}&k={SectionCode}-{CemeteryCode}&sel=2";
+                }
+            }
+        }
+
         public class PageCemeteryInfo
         {
             public int CemeteryInfoIndex { get; set; }
@@ -114,7 +174,10 @@ namespace YasiroRegrave.Pages
             public string CemeteryName { get; set; }
             public string Image1Fname { get; set; }
             public string Image2Fname { get; set; }
-
+            public string ReienCode { get; set; }
+            public string AreaCode { get; set; }
+            public string SectionCode { get; set; }
+            public string CemeteryCode { get;set; }
         }
     }
 }
